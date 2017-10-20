bb511f986a892ab305dfb9f471a5b882c77b061e
Sam Zhao <sam@cs.brown.edu>
2017-10-20 16:39:47 -0400

    third_party: scidb find java7

--- third_party/scidb-14.12/cmake/Modules/FindJava7.cmake
+++ third_party/scidb-14.12/cmake/Modules/FindJava7.cmake
@@ -1,8 +1,8 @@
 INCLUDE(FindPackageHandleStandardArgs)
 
 set(_JAVA_PATHS
-  /usr/lib/jvm/java-1.6.0-openjdk-amd64/bin
-  /usr/lib/jvm/java-1.6.0-openjdk.x86_64/bin
+  /usr/lib/jvm/java-1.7.0-openjdk-amd64/bin
+  /usr/lib/jvm/java-1.7.0-openjdk.x86_64/bin
   )
 
 find_program(Java_JAVA_EXECUTABLE
@@ -34,13 +34,13 @@ find_program(Java_JAVADOC_EXECUTABLE
    NO_DEFAULT_PATH
 )
 
-find_package_handle_standard_args(Java6
+find_package_handle_standard_args(Java7
         REQUIRED_VARS Java_JAVA_EXECUTABLE Java_JAR_EXECUTABLE Java_JAVAC_EXECUTABLE
                       Java_JAVAH_EXECUTABLE Java_JAVADOC_EXECUTABLE
-        VERSION_VAR Java6_VERSION
+        VERSION_VAR Java7_VERSION
         )
 
-string(REGEX REPLACE "/bin/javac$" "" Java6_HOME ${Java_JAVAC_EXECUTABLE})
+string(REGEX REPLACE "/bin/javac$" "" Java7_HOME ${Java_JAVAC_EXECUTABLE})
 
 mark_as_advanced(
   Java_JAVA_EXECUTABLE
